7d3cf48df9194877d27d96e4339f2bb8770467b2
kryostar <1kryostar@gmail.com>
2021-01-23 13:01:41 +0530

    -

--- notes/views.py
+++ notes/views.py
@@ -1,36 +1,49 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import *
-from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth import authenticate, logout, login
+
 
 # Create your views here.
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
+
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
 def contact(request):
-    return render(request,'contact.html')
+    return render(request, 'contact.html')
+
+
 def login1(request):
-    return render(request,'login1.html')
+    return render(request, 'login1.html')
+
+
 def admin1(request):
-    error=""
-    if request.method=='POST':
-        u=request.POST['uname']
-        p=request.POST['pwd']
-        user=authenticate(username=u, password=p)
+    error = ""
+    if request.method == 'POST':
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        user = authenticate(username=u, password=p)
         try:
             if user.is_staff:
                 login(request, user)
-                error="no"
+                error = "no"
             else:
-                error="yes"
+                error = "yes"
         except:
-                error="yes"
-    d={'error': error}
-    return render(request,'admin1.html')
+            error = "yes"
+    d = {'error': error}
+    return render(request, 'admin1.html')
+
+
 def signup(request):
-    return render(request,'signup.html')
+    return render(request, 'signup.html')
+
+
 def admin_home(request):
     if not request.user.is_staff:
-        return redirect('login_admin')
-    return render(request,'admin_home.html')
+        return redirect('admin1.html')
+    return render(request, 'admin_home.html')
